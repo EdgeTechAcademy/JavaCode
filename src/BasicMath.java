89fce0f805c1549eaa57f81b79260e16376eb52d
gtjames <gjames@ancoraeducation.com>
2016-11-09 14:58:05 -0600

    create

--- src/BasicMath.java
+++ src/BasicMath.java
@@ -14,5 +14,15 @@ public class BasicMath {
         System.out.printf("The rint of %.2f " + "is %.0f%n",            b, Math.rint(b));
         System.out.printf("The max of %d and " + "%d is %d%n",       c, d, Math.max(c, d));
         System.out.printf("The min of of %d " + "and %d is %d%n",    c, d, Math.min(c, d));
+
+        double x = 11.635;
+        double y = 2.76;
+
+        System.out.printf("The value of " + "e is %.4f%n",         Math.E);
+        System.out.printf("exp(%.3f) " + "is %.3f%n",           x, Math.exp(x));
+        System.out.printf("log(%.3f) is " + "%.3f%n",           x, Math.log(x));
+        System.out.printf("pow(%.3f, %.3f) " + "is %.3f%n",  x, y, Math.pow(x, y));
+        System.out.printf("sqrt(%.3f) is " + "%.3f%n",          x, Math.sqrt(x));
+
     }
 }
