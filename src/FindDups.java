0fa2e6d3de65aa9569ed1b94baf7217495c37d99
gtjames <gjames@ancoraeducation.com>
2017-03-02 07:54:50 -0600

    create

---
+++ src/FindDups.java
@@ -0,0 +1,14 @@
+/**
+ * Created by Edge Tech Academy on 3/1/2017.
+ */
+
+import java.util.*;
+
+public class FindDups {
+	public static void main(String[] args) {
+		Set<String> s = new HashSet<String>();
+		for (String a : args)
+			s.add(a);
+		System.out.println(s.size() + " distinct words: " + s);
+	}
+}
