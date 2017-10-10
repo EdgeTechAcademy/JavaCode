3002b5f3f839ec8654a739469ef5ef2608a75b5c
gtjames <gjames@ancoraeducation.com>
2017-10-10 07:11:44 -0500

    create

---
+++ src/FirstClass.java
@@ -0,0 +1,20 @@
+/**
+ * Created by Edge Tech Academy on 10/9/2017.
+ */
+public class FirstClass {
+	public static void main (String[] args) {
+		System.out.println("Hello World!");
+
+		int number = 37;
+		System.out.println("This is a number " + number);
+
+		char character = 'z';
+		System.out.println("this is a character " + character);
+
+		double pi = Math.PI;
+		System.out.println("The famous number PI " + pi);
+
+		for ( char x = 'A'; x < 'Z'; x++)
+			System.out.println("This is the character " + x + " This is the value of x " + (byte)x);
+	}
+}
