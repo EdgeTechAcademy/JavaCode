90dc07bbba65bde4de4fd97d3ded493743693e05
gtjames <gjames@ancoraeducation.com>
2017-10-17 07:36:07 -0500

    create

--- src/Deal.java
+++ src/Deal.java
@@ -14,14 +14,14 @@ public class Deal {
 
 		// Make a normal 52-card deck.
 		String[] suit = new String[] {
-				"spades", "hearts", "diamonds", "clubs" };
+				"♣", "♥", "♦", "♠" };
 		String[] rank = new String[] {
-				"ace","2","3","4","5","6","7","8",
-				"9","10","jack","queen","king" };
+				"A","2","3","4","5","6","7","8",
+				"9","10","J","Q","K" };
 		List<String> deck = new ArrayList<String>();
 		for (int i = 0; i < suit.length; i++)
 			for (int j = 0; j < rank.length; j++)
-				deck.add(rank[j] + " of " + suit[i]);
+				deck.add(rank[j] + suit[i]);
 
 		// Shuffle the deck.
 		Collections.shuffle(deck);
--- src/FirstClass.java
+++ src/FirstClass.java
@@ -1,20 +1,46 @@
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
 /**
  * Created by Edge Tech Academy on 10/9/2017.
  */
 public class FirstClass {
 	public static void main (String[] args) {
-		System.out.println("Hello World!");
 
-		int number = 37;
-		System.out.println("This is a number " + number);
+		String rick = "Louie, I think this is the beginning of a beautiful friendship.";
+		System.out.println(rick + " length " + rick.length());
+		String renault = "I'm shocked, shocked to find that gambling is going on in here!";
+		System.out.println("char 13 " + renault.charAt (13));
+		renault = renault.replaceFirst("shocked", "SHOCKED");
+		System.out.println(renault);
+		System.out.println("Play it, Sam. Play ‘As Time Goes By.’".substring (19, 35));
+		System.out.println("Play it, Sam. Play ‘As Time Goes By.’".substring (19, 35).length());
 
-		char character = 'z';
-		System.out.println("this is a character " + character);
+		String original = "Java Software";
+		int   space  = original.indexOf(' ');
+		String soft  = original.substring(space+1,space+5);
+		String ware  = original.substring(9);
+		System.out.println(ware.toUpperCase() + " " + soft.toLowerCase());
 
-		double pi = Math.PI;
-		System.out.println("The famous number PI " + pi);
+		String[] words = "This is a string with how many words".split(" ");
+		System.out.println("this is the 4th word in the string " + words[3]);
+		System.out.println("The string had how many words? " + words.length);
 
-		for ( char x = '0'; x < '9'; x++)
-			System.out.println("This is the character " + x + " This is the value of x " + (byte)x);
+		String replace = original.replace("a","_");
+		System.out.println(replace);
+		System.out.println(original.replaceFirst("a","_"));
+
+		String name = "Gary Thomas James";
+		String intials;
+		String[] names = name.split(" ");
+		String initials = Arrays.stream(names).map(n -> n.substring(0,1)).collect(Collectors.joining(""));
+		System.out.println(initials.toLowerCase());
+
+		String barbara = "She said, \"Oh, no you don’t\"\nand I said, “Oh yes I did\"";
+		System.out.println(barbara);
+		barbara = barbara.replace("She", "Barbara");
+		barbara = barbara.replace("did", "DID!");
+		barbara = barbara.replace("Oh yes", "Oh, yes");
+		System.out.println(barbara);
 	}
 }
