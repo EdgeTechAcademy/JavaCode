d7cfd79df68e4bf0f93f0f3e01f1baabea3a2aae
edgetechacademy <gjames@ancoraeducation.com>
2018-03-30 12:35:05 -0500

    general updates

--- src/Collect.java
+++ src/Collect.java
@@ -7,39 +7,45 @@ public class Collect {
 
         List<String> list = new ArrayList<>();
         Set<String> set = new HashSet<>();
-        HashMap<Integer, String> map = new HashMap<>();
+        Map<Integer, String> map = new HashMap<>();
         int cnt = 0;
-        while (true) {
 
+        while (true) {
             System.out.print("Enter data: ");
             String str = sc.nextLine();
             switch (str) {
                 case "show":
                     System.out.println(list.size() + ": list = " + list);
-                    System.out.println(set.size() + ": set = " + set);
-                    System.out.println(map.size() + ": map = " + map);
+                    System.out.println(set.size() +  ": set  = " + set);
+                    System.out.println(map.size() +  ": map  = " + map);
                     continue;
+
                 case "map":
                     Set keys = map.keySet();
                     Collection values = map.values();
-                    System.out.println("keys = " + keys);
-                    System.out.println("values = " + values);
+                    Set reduced = new HashSet(values);
+                    System.out.println("keys    = " + keys);
+                    System.out.println("values  = " + values);
+                    System.out.println("reduced = " + reduced);
                     break;
+
                 case "join":
                     String joined = list.stream()
                                         .map(Object::toString)
                                         .collect(Collectors.joining(", "));
                     System.out.println("joined = " + joined);
                     break;
+
                 case "stop":
+                    return;
+
+                default:
+//                    Collections.sort(list);
+                    list.add(str);
+                    set.add(str);
+                    map.put(cnt++, str);
                     break;
             }
-
-            Collections.sort(list);
-            list.add(str);
-            set.add(str);
-            map.put(cnt++, str);
         }
-
     }
 }
