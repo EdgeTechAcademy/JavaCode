4b8d42ffa592933c1034ebd58790d1453430d6bc
edgetechacademy <gjames@ancoraeducation.com>
2019-01-14 07:45:16 -0600

    general updates

--- src/Collect.java
+++ src/Collect.java
@@ -11,40 +11,39 @@ public class Collect {
         int cnt = 0;
 
         while (true) {
-            System.out.print("Enter data: ");
+            System.out.print("Enter data: (show, map, join, stop, data to add to Collections)\n-> ");
             String str = sc.nextLine();
             switch (str) {
-                case "show":
-                    System.out.println(list.size() + ": list = " + list);
-                    System.out.println(set.size() +  ": set  = " + set);
-                    System.out.println(map.size() +  ": map  = " + map);
-                    continue;
+            case "show":
+                System.out.println(list.size() + ": list = " + list);
+                System.out.println(set.size() + ": set  = " + set);
+                System.out.println(map.size() + ": map  = " + map);
+                continue;
 
-                case "map":
-                    Set keys = map.keySet();
-                    Collection values = map.values();
-                    Set reduced = new HashSet(values);
-                    System.out.println("keys    = " + keys);
-                    System.out.println("values  = " + values);
-                    System.out.println("reduced = " + reduced);
-                    break;
+            case "map":
+                Set keys = map.keySet();
+                Collection values = map.values();
+                Set reduced = new HashSet(values);
+                System.out.println("keys    = " + keys);
+                System.out.println("values  = " + values);
+                System.out.println("reduced = " + reduced);
+                break;
 
-                case "join":
-                    String joined = list.stream()
-                                        .map(Object::toString)
-                                        .collect(Collectors.joining(", "));
-                    System.out.println("joined = " + joined);
-                    break;
+            case "join":
+                String joined = list.stream().map(Object::toString).collect(Collectors.joining(", "));
+                System.out.println("joined = " + joined);
+                break;
 
-                case "stop":
-                    return;
+            case "stop":
+                sc.close();
+                return;
 
-                default:
-//                    Collections.sort(list);
-                    list.add(str);
-                    set.add(str);
-                    map.put(cnt++, str);
-                    break;
+            default:
+                // Collections.sort(list);
+                list.add(str);
+                set.add(str);
+                map.put(cnt++, str);
+                break;
             }
         }
     }
--- src/TrigDemo.java
+++ src/TrigDemo.java
@@ -13,10 +13,11 @@ public class TrigDemo {
         System.out.format("The sine    of %.1f " + "degrees is %.4f%n", degrees, Math.sin(radians));
         System.out.format("The cosine  of %.1f " + "degrees is %.4f%n", degrees, Math.cos(radians));
         System.out.format("The tangent of %.1f " + "degrees is %.4f%n", degrees, Math.tan(radians));
+		String wave = "-------------------+....................";
 
-        for ( double d = 0.0; d <= 2 * Math.PI; d += 0.1) {
-            int sinValue = (int)(Math.cos(d) * 20) + 20;
-            String histogram = "-------------------+....................".substring(0, sinValue);
+        for ( double d = 0.0; d <= 2 * Math.PI*2; d += 0.1) {
+            int sinValue = (int)(Math.cos(d) * wave.length()/2) + wave.length()/2;
+            String histogram = wave.substring(0, sinValue);
             System.out.format("Degrees %3d %5.2f %s %n", (int)Math.toDegrees(d), Math.cos(d), histogram);
         }
     }
